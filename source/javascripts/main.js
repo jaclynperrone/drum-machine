e671366b5eab85c994c5e2078b3dc4c96d10a804
Jaclyn Perrone <pair+jperrone@pivotallabs.com>
2015-02-11 17:15:18 -0500

    Merge in object refactor.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,18 +1,39 @@
 $(function(){
 
   var totalBeats = 16;
-  var hiHat = [];
-  var kick = [];
-  var snare = [];
   var bpm = 120;
   var speed = (60000 / bpm);
 
+  var Sound = function(audioFileID, pattern) {
+    var self = this;
+    self.audioFileID = audioFileID;
+    self.pattern = pattern;
+    self.audioElement = document.getElementById(self.audioFileID);
+
+    self.play = function(){
+      console.log(self.audioElement);
+      self.audioElement.currentTime = 0;
+      self.audioElement.play();
+    }
+
+    self.toggleSound = function(status) {
+      if (self.pattern[status] == 0) {
+        self.pattern[status] = 1;
+      } else {
+        self.pattern[status] = 0;
+      }
+    }
+  }
+
+  var kick = new Sound('kick-sound', []);
+  var hihat = new Sound('hihat-sound', []);
+  var snare = new Sound('snare-sound', []);
 
   function init() {
     for(var i = 0; i < totalBeats; i++) {
-      hiHat.push(0);  
-      kick.push(0);
-      snare.push(0);
+      hihat.pattern.push(0);  
+      kick.pattern.push(0);
+      snare.pattern.push(0);
 
       generateSquares('.kick', i);
       generateSquares('.hihat', i);
@@ -47,6 +68,28 @@ $(function(){
   });
 
   init();
+  // kick.selectSound();
+
+$('.kick td').click(function(){
+  var squarePosition = $(this).attr('rel');
+  $(this).toggleClass('on');
+  kick.toggleSound(squarePosition);
+  // console.log(kick.pattern);
+});
+
+$('.snare td').click(function(){
+  var squarePosition = $(this).attr('rel');
+  $(this).toggleClass('on');
+  snare.toggleSound(squarePosition);
+  // console.log(kick.pattern);
+});
+
+$('.hihat td').click(function(){
+  var squarePosition = $(this).attr('rel');
+  $(this).toggleClass('on');
+  hihat.toggleSound(squarePosition);
+  // console.log(kick.pattern);
+});
 
   //timer
   var beat = 0;
@@ -55,25 +98,17 @@ $(function(){
     var beatID = 'beat'+beat.toString();
     // console.log(beat);
 
-    //add color
     $('#'+beatID).addClass('on').siblings().removeClass('on');
 
-    //play sound
-    if (hiHat[beat] == 1) {
-      // console.log('HIHAT');
-      document.getElementById('hihat-sound').currentTime=0;
-      document.getElementById('hihat-sound').play();
+    if (hihat.pattern[beat] == 1) {
+      hihat.play();
     }
 
-    if (kick[beat] == 1) {
-      // console.log('KICK');
-      document.getElementById('kick-sound').currentTime=0;
-      document.getElementById('kick-sound').play();
+    if (kick.pattern[beat] == 1) {
+      kick.play();
     }
-    if (snare[beat] == 1) {
-      // console.log('KICK');
-      document.getElementById('snare-sound').currentTime=0;
-      document.getElementById('snare-sound').play();
+    if (snare.pattern[beat] == 1) {
+      snare.play();
     }
 
     beat++;
@@ -81,35 +116,5 @@ $(function(){
       beat = 0;
     }
   }
-
-  var kickSquare = $('.kick td');
-  var hiHatSquare = $('.hihat td');
-  var snareSquare = $('.snare td');
-
-  function selectBeat(soundSquare, array) {
-    $(soundSquare).click(function(){
-      var squarePosition = $(this).attr('rel');
-
-      $(this).toggleClass('on');
-
-      if (array[squarePosition] == 0) {
-          array[squarePosition] = 1;
-      } else {
-        array[squarePosition] = 0;
-      }
-      // console.log(array);
-   });
-  }
-
-  selectBeat(kickSquare, kick);
-  selectBeat(hiHatSquare, hiHat);
-  selectBeat(snareSquare, snare);
-
-
-  //comments
-  //refactor sequencer into array
-  //fireBeat function is getting big
-  //change sequence to rel
-
 
 });
